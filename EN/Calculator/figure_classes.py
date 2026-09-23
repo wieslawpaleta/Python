bd8d50db46501c24e978aed9d9fdbec443856fd3
wieslawpaleta <tankluk@gmail.com>
2026-09-23 20:33:37 +0200

    Add files via upload

--- EN/Calculator/figure_classes.py
+++ EN/Calculator/figure_classes.py
@@ -1,74 +1,82 @@
 import math
 
 
-class Rectangle:
+class ClassicRectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
 
-
-    def areaR(self):
+    def areaCR(self):
         calculation = self.length * self.width
         return f"The area of your rectangle is: {calculation}" 
 
+your_rectangle = ClassicRectangle(5, 6)
 
-your_rectangle = Rectangle(5, 6)
+print(your_rectangle.areaCR())
 
-print(your_rectangle.areaR())
-
-class Square:
+class PowSquare:
     def __init__(self, length):
         self.length = length
 
-
-    def areaS(self):
+    def areaPS(self):
         calculation = pow(self.length, 2)
         return f"The area of your square is: {calculation}"
 
+your_powsquare = PowSquare(5)
 
-your_square = Square(5)
+print(your_powsquare.areaPS())
 
-print(your_square.areaS())
-
-class EquilateralTriangle1:
-    def __init__(self, side):
-        self.side = side    
-
-
-    def areaET(self):
-        calculation = round(self.side ** 2 * 3 ** (1/2) / 4, 4)
-        return f"The area of your equilateral triangle is: {calculation}"
-
-
-your_triangleET = EquilateralTriangle1(5)
-
-print(your_triangleET.areaET())
 
 class ClassicTriangle:
     def __init__(self, base, height):
         self.base = base
         self.height = height
 
-
     def areaCT(self):
         calculation = round(self.base * self.height / 2, 4)
         return f"The area of your triangle is: {calculation}"
-
 
 your_triangleCT = ClassicTriangle(4, 5)
 
 print(your_triangleCT.areaCT())
 
-class TriangleHeight:
+
+class EquilateralTriangleA234:
+    def __init__(self, side):
+        self.side = side    
+
+    def areaETA234(self):
+        calculation = round(self.side ** 2 * 3 ** (1/2) / 4, 4)
+        return f"The area of your equilateral triangle is: {calculation}"
+
+your_triangleET = EquilateralTriangleA234(5)
+
+print(your_triangleET.areaETA234())
+
+
+class EquilateralTriangleHeight:
     def __init__ (self, height):
         self.height = height
 
-
-    def areaTH(self):
+    def areaETH(self):
         calculation = round((self.height ** 2) * (3 ** (1/2)) / 3, 4)
         return f"The area of your triangle is: {calculation}"
 
+your_triangleTH = EquilateralTriangleHeight(5)
 
-your_triangleTH = TriangleHeight(5)
+print(your_triangleTH.areaETH())
 
-print(your_triangleTH.areaTH())
+
+class EquilateralCircumscribedCircleTriangle:
+    def __init__ (self, Radius):
+        self.Radius = Radius
+
+    def areaECCT(self):
+        calculation = round((3 * self.Radius ** 2) * 2 ** (1/2) / 4, 4)
+        return f"The area of your triangle is: {calculation}"
+
+your_triangleICT = EquilateralCircumscribedCircleTriangle(5)
+
+print(your_triangleICT.areaECCT())
+
+
