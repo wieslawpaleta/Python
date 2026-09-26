003898096e0eb4ac620c873c7fa82e6e0f62a44b
wieslawpaleta <tankluk@gmail.com>
2026-09-26 21:20:20 +0200

    Add files via upload

--- Practice/practice.py
+++ Practice/practice.py
@@ -1011,14 +1011,89 @@
 # myset = set1 | set2 | set3 | set4
 # print(myset)
 
-x = {"a", "b", "c"}
-y = (1, 2, 3)
+# x = {"a", "b", "c"}
+# y = (1, 2, 3)
 
-z = x.union(y)
-print(z)
+# z = x.union(y)
+# print(z)
 
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
 
-set1.update(set2)
-print(set1)
+# set1.update(set2)
+# print(set1)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1 & set2
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set1.intersection_update(set2)
+
+# print(set1)
+
+# set1 = {"apple", 1,  "banana", 0, "cherry"}
+# set2 = {False, "google", 1, "apple", 2, True}
+
+# set3 = set1.intersection(set2)
+
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1.difference(set2)
+
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1 - set2
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set1.difference_update(set2)
+
+# print(set1)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1.symmetric_difference(set2)
+
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set3 = set1 ^ set2
+# print(set3)
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+
+# set1.symmetric_difference_update(set2)
+
+# print(set1)
+
+
+##Sets/Frozenset
+# x = frozenset({"apple", "banana", "cherry"})
+# print(x)
+# print(type(x))
+
+
+##Sets/Set Methods
